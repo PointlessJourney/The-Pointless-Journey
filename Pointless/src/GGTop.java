038ecb5620bc1d1beb451968d26c71f4affb900a
694694 <694694@2452IR306230423.ad1.peelsb.com>
2017-05-10 08:33:49 -0400

    s

--- Pointless/src/GGTop.java
+++ Pointless/src/GGTop.java
@@ -12,11 +12,11 @@ public class GGTop extends OverChar {
 	static int count = 0;
 	boolean first = true;
 	boolean second = true;
+	int size = 25;
 	int maxX = 520;
 	int maxNegX =  -4276;
 	int maxY = 156;
 	int maxNegY = -4422;
-	int size = 25;
 	BufferedImage map = LoadImage("basemap.png");
 	
 	public enum STATE {			// states of map areas (base, field, sidescroller map, etc)
@@ -24,7 +24,7 @@ public class GGTop extends OverChar {
 		Base
 	};
 
-	public STATE mapState = STATE.Field;
+	public STATE mapState = STATE.Base;
 
 	public GGTop(int x, int y, ID player) {
 		super(x, y, player);
@@ -32,15 +32,10 @@ public class GGTop extends OverChar {
 	}
 
 	public void tick() {
+		x += velX;							// moves the character around
+		y += velY;
 
-		//if(x!=0&&y!=0){
-		//	x+=Math.sqrt(2)*(velX);// moves the character around
-		//	y+=Math.sqrt(2)*(velY);
-		//}else{
-			x += velX;
-			y += velY;
-		//}
-		if (x >= maxX) x = maxX;
+		if (x >= maxX) x = maxX;			// boundary detection 
 		if (x <= maxNegX) x = maxNegX;
 		if (y >= maxY) y = maxY;
 		if (y <= maxNegY) y = maxNegY;
@@ -49,59 +44,8 @@ public class GGTop extends OverChar {
 		{
 			if (x == -4276 && y == -3980 )	// entering the battle field area
 			{
-				mapState = STATE.Field;			
-			}
-
-		}
-		else if (mapState == STATE.Field)
-		{
-			if (x <= -8164 && x >= -8300 && y >= -2654 && y <= -2324)	// returning to the main area
-			{
-				mapState = STATE.Base;	
-			}
-
-
-		}
-		playerX=x;//updating the static references to player location. these should never be changed anywhere else
-		playerY=y;
-
-
-
-
-	}
-
-	public void render(Graphics g) {	// enter character picture and information here
-
-		
-		if(id == ID.Map)		//creates the map
-		{
-			if(mapState == STATE.Base)
-			{
-				size = 25;
-				maxX = 520;
-				maxNegX = -4276;
-				maxY = 156;
-				maxNegY = -4422;
-				if (first)
-				{
-					x = -112;
-					y = -1120;
-					map = LoadImage("basemap.png");
-					System.out.println("start");
-					first = false;
-					second = true;
-				}
-
+				mapState = STATE.Field;
 				
-			}
-
-			if(mapState == STATE.Field)
-			{
-				size = 7;
-				maxX = -34;
-				maxNegX = -8300;
-				maxY = -14;
-				maxNegY = -4664;
 				if (second)
 				{
 					x = 0;
@@ -112,9 +56,56 @@ public class GGTop extends OverChar {
 				}
 			}
 
+		}
+		else if (mapState == STATE.Field)
+		{
+			if (x <= -8164 && x >= -8300 && y >= -2654 && y <= -2324)	// returning to the main area
+			{
+				mapState = STATE.Base;
+				
+				if (first)
+				{
+					x = -112;
+					y = -1120;
+					map = LoadImage("basemap.png");
+					System.out.println("start");
+					first = false;
+					second = true;
+				}
+			}
+
+		}
+
+
+
+
+	}
+
+	public void render(Graphics g) {	// enter character picture and information here
+
+		if(id == ID.Map)		//creates the map
+		{
+			if(mapState == STATE.Base)
+			{
+				size = 25;
+				maxX = 520;
+				maxNegX =  -4276;
+				maxY = 156;
+				maxNegY = -4422;
+			}
+
+			if(mapState == STATE.Field)
+			{
+				size = 7;
+				maxX = -34;
+				maxNegX =  -8300;
+				maxY =-14;
+				maxNegY = -4664;
+				
+			}
+
 			AffineTransform at = AffineTransform.getTranslateInstance(x, y);	// moves the picture around
-			at.scale(size/1280.0*MainMenu.width,size/1280.0*MainMenu.width);
-			//at.scale(25, 25);
+			at.scale(size,size);			// scales it (multiplied by the size)
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.drawImage(map, at, null);		// draws it
 
@@ -122,17 +113,17 @@ public class GGTop extends OverChar {
 
 		else if (id == ID.Player) // creates the player
 		{
-			
-			int mouseY = MouseInfo.getPointerInfo().getLocation().y;
+			int centerX = 576;
+			int centerY = 296;
+			int mouseY = MouseInfo.getPointerInfo().getLocation().y;		// mouse tracking
 			int mouseX = MouseInfo.getPointerInfo().getLocation().x;
-			//int shift = 20;
+			int shift = 20;
 
-			
+			double angle = Math.atan2(centerY - mouseY, centerX - mouseX) - Math.PI/2;
 
+			AffineTransform tat = AffineTransform.getTranslateInstance((1280/2)-shift, (720/2)-shift);
 			BufferedImage map = LoadImage("GGtop.png");
-			AffineTransform tat = AffineTransform.getTranslateInstance((MainMenu.width/2-map.getWidth()), MainMenu.height/2-map.getHeight());
-			tat.scale(2.0/1280.0*MainMenu.width, 2.0/1280.0*MainMenu.width);
-			double angle = Math.atan2(MainMenu.height/2.0 - mouseY, MainMenu.width/2.0 - mouseX) - Math.PI/2;
+			tat.scale(2, 2);
 			tat.rotate(angle, map.getWidth()/2, map.getHeight()/2);
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.drawImage(map, tat, null);	
@@ -141,6 +132,18 @@ public class GGTop extends OverChar {
 
 
 	}
-	
+	BufferedImage LoadImage(String FileName)
+	{
+		BufferedImage img = null;
+		try
+		{
+			img = ImageIO.read(new File(FileName));
+		}
+		catch (IOException e)
+		{
+
+		}
+		return img;
+	}
 
 }
