6660b8ebf36cd0d1440b01b9421b220cea1c24c2
Mrbeadd <jjwindross@gmail.com>
2017-05-15 13:39:08 -0400

    fixing render order (#59)

--- Pointless/src/KeyIn.java
+++ Pointless/src/KeyIn.java
@@ -25,7 +25,8 @@ public class KeyIn extends KeyAdapter {
 				if (key == KeyEvent.VK_S)OverChar.setVelY(down);
 				if (key == KeyEvent.VK_A)OverChar.setVelX(left);		
 				if (key == KeyEvent.VK_D)OverChar.setVelX(right);
-				if(key==KeyEvent.VK_C)System.out.println("x:" + OverChar.playerX + "    y:"+OverChar.playerY+"  "+MouseInfo.getPointerInfo().getLocation().x+"  "+MouseInfo.getPointerInfo().getLocation().y);
+				if(key==KeyEvent.VK_C)System.out.println("x:" + OverChar.playerX + "    y:"+OverChar.playerY+"  mx:"+MouseInfo.getPointerInfo().getLocation().x+"  my:"+MouseInfo.getPointerInfo().getLocation().y);
+				
 
 			//System.out.println(OverChar.playerX);
 				/*if(e.isShiftDown()){
