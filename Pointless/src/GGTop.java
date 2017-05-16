b6957eaf652bc983e78fcf8711904ac2a79c1127
Mrbeadd <jjwindross@gmail.com>
2017-05-16 09:11:06 -0400

    fix

--- Pointless/src/GGTop.java
+++ Pointless/src/GGTop.java
@@ -58,11 +58,12 @@ public class GGTop extends OverChar {
 			}else{
 				OverChar.playerX += velX;
 				OverChar.playerY += velY;
-				if (playerX <= maxX) playerX = maxX;
-				if (playerX >= maxNegX) playerX = maxNegX;
-				if (playerY <= maxY) playerY = maxY;
-				if (playerY >= maxNegY) playerY = maxNegY;
 			}
+
+			if (playerX <= maxX) playerX = maxX;
+			if (playerX >= maxNegX) playerX = maxNegX;
+			if (playerY <= maxY) playerY = maxY;
+			if (playerY >= maxNegY) playerY = maxNegY;
 			return;
 		}
 		if (mapState == STATE.Base)
