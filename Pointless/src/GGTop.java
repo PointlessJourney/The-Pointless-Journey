121f427bd2214b4de080fd6a7a9e4f3c0adbb9c4
497773 <497773@2452IR306173947.ad1.peelsb.com>
2017-05-19 13:38:20 -0400

    New Update 5/19/17

--- Pointless/src/GGTop.java
+++ Pointless/src/GGTop.java
@@ -7,6 +7,7 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 public class GGTop extends OverChar {
@@ -20,6 +21,9 @@ public class GGTop extends OverChar {
 	int delay=0;
 	int ranX;//for random enemy x
 	int ranY;//for random enemy y
+	
+	//Random ranX = new Random();
+	//Random ranY = new Random();
 
 
 
@@ -111,13 +115,13 @@ public class GGTop extends OverChar {
 				
 				delay = (int)(Math.random() * 1200.0);
 				//96x96
-				for (int w = 0; w > 10; w++)
-				{
+				//for (int w = 0; w > 10; w++)
+				//{
 				ranX = (int) (Math.random() * MainMenu.width -96);
 				ranY = (int) (Math.random() * MainMenu.height -96);
 				Handler.addObject(new RandomSpawner(ranX,ranY,ID.RandomEnemy,LoadImage("bad guy clone.gif") ,96.0));
 				System.out.println("Spawned" + ranX + ranY);
-				}
+				//}
 
 			}
 			else
