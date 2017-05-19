ed9a42e762b68bd80454e6fe164ddb95a0147fca
694694 <694694@2452IR306183541.ad1.peelsb.com>
2017-05-19 14:06:40 -0400

    temporary

--- Pointless/src/MainMenu.java
+++ Pointless/src/MainMenu.java
@@ -11,12 +11,13 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Point;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineEvent;
